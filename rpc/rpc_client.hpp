be79a46a5e565c98cc96893da9e610ee4be3b643
Yiwen Zhang <bessss-zyw@sjtu.edu.cn>
2022-04-29 09:14:02 +0800

    minor

--- rpc/rpc_client.hpp
+++ rpc/rpc_client.hpp
@@ -10,6 +10,9 @@
 #define MAX_TIMEOUT rpc_const::to_max
 
 static void *poll_thread(void *arg);
+static int cmp_timespec(const struct timespec &a, const struct timespec &b);
+static void add_timespec(const struct timespec &a, int b, struct timespec *result);
+static int diff_timespec(const struct timespec &end, const struct timespec &start);
 
 // manages per RPC info
 struct caller {
@@ -266,4 +269,41 @@ static void *poll_thread(void *arg)
 		if (errno == EINTR) break;
 	}
     return NULL;
+}
+
+static int cmp_timespec(const struct timespec &a, const struct timespec &b) {
+	if(a.tv_sec > b.tv_sec)
+		return 1;
+	else if(a.tv_sec < b.tv_sec)
+		return -1;
+	else {
+		if(a.tv_nsec > b.tv_nsec)
+			return 1;
+		else if(a.tv_nsec < b.tv_nsec)
+			return -1;
+		else
+			return 0;
+	}
+}
+
+static void add_timespec(const struct timespec &a, int b, struct timespec *result) {
+	// convert to millisec, add timeout, convert back
+	result->tv_sec = a.tv_sec + b/1000;
+	result->tv_nsec = a.tv_nsec + (b % 1000) * 1000000;
+	VERIFY(result->tv_nsec >= 0);
+	while (result->tv_nsec > 1000000000){
+		result->tv_sec++;
+		result->tv_nsec-=1000000000;
+	}
+}
+
+static int diff_timespec(const struct timespec &end, const struct timespec &start) {
+	int diff = (end.tv_sec > start.tv_sec)?(end.tv_sec-start.tv_sec)*1000:0;
+	VERIFY(diff || end.tv_sec == start.tv_sec);
+	if(end.tv_nsec > start.tv_nsec){
+		diff += (end.tv_nsec-start.tv_nsec)/1000000;
+	} else {
+		diff -= (start.tv_nsec-end.tv_nsec)/1000000;
+	}
+	return diff;
 }
